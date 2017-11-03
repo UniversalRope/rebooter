e525f4f5e920926f90e0a1c9c9c687f150ccf9b8
UniversalRope <Promvv@gmail.com>
2017-11-04 01:16:50 +0300

    Первая ночь

---
+++ rebooter/mainmenu.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+OPTION=$(whiptail --title "Меню обслуживания рабочей станции" --menu "Сделайте ваш выбор" 14 60 6 --notags --nocancel \
+"1" "Состояние сети" \
+"2" "Выполнить сценарий" \
+"3" "Резервное восстановление системы" \
+"4" "Резервное копирование системы" \
+"5" "Дополнительные возможности" \
+"6" "Завершение работы"  3>&1 1>&2 2>&3)
+
+case $OPTION in
+1)
+bash $(pwd)/network.bash
+exit 0
+;;
+2)
+bash $(pwd)/scenario.bash
+exit 0
+;;
+3)
+bash $(pwd)/restorer.bash
+exit 0
+;;
+4)
+bash $(pwd)/backuper.bash
+exit 0
+;;
+5)
+bash $(pwd)/anotheroptions
+exit 0
+;;
+6)
+
+	OPTION2=$(whiptail --title "Завершение работы" --menu "Сделайте ваш выбор" 10 60 2 --notags --cancel-button Назад \
+	"1" "Выполнить завершение работы" \
+	"2" "Выполнить выполнить перезагрузку" 3>&1 1>&2 2>&3)
+	exitstatus=$?
+
+	if [ $exitstatus = 0 ]; then
+
+		case $OPTION2 in
+		1)
+		echo "sudo shutdown -h now"
+		;;
+		2)
+		echo "sudo reboot"
+		;;
+		esac
+	else
+		bash $(pwd)/mainmenu.bash
+		exit 0
+	fi
+	exit 0
+;;
+esac
+
---
+++ rebooter/network.bash
@@ -0,0 +1,8 @@
+#!/bin/bash/
+
+tmp=$(mktemp)
+
+echo "Интерфейс            MAC              IP" >> $tmp
+echo "Интерфейс            MAC              IP" >> $tmp
+
+nano $tmp
